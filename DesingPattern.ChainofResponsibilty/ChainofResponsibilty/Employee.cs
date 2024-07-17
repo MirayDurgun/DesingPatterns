18ae2eebdc84a1dead90cbc545ec4e27f76d950a
Miray Durgun <miraydurgun67@gmail.com>
2024-07-17 09:47:16 +0300

    6. video sonu

---
+++ DesingPattern.ChainofResponsibilty/ChainofResponsibilty/Employee.cs
@@ -0,0 +1,14 @@
+﻿using DesingPattern.ChainofResponsibilty.Models;
+
+namespace DesingPattern.ChainofResponsibilty.ChainofResponsibilty
+{
+	public abstract class Employee
+	{
+		protected Employee NextApprover;
+		public void SetNextApprover(Employee superVisor)
+		{
+			this.NextApprover = superVisor;
+		}
+		public abstract void ProcessRequest(CustomerProcessViewModel req);
+	}
+}
---
+++ DesingPattern.ChainofResponsibilty/Models/CustomerProcessViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DesingPattern.ChainofResponsibilty.Models
+{
+	public class CustomerProcessViewModel
+	{
+		public int CustomerProssesID { get; set; }
+		public string Name { get; set; }
+		public int Amount { get; set; }
+		public string EmployeeName { get; set; }
+		public string Description { get; set; }
+	}
+}
